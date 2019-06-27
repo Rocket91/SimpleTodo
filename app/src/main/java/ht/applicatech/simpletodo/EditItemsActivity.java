86742b27a12b5d3fe61413cfa500d68471c2e18e
Rocket91 <jeancharlesyvenson@hotmail.com>
2019-06-27 12:44:56 -0400

    add edit item

---
+++ app/src/main/java/ht/applicatech/simpletodo/EditItemsActivity.java
@@ -0,0 +1,48 @@
+package ht.applicatech.simpletodo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static ht.applicatech.simpletodo.MainActivity.ITEM_POSITION;
+import static ht.applicatech.simpletodo.MainActivity.ITEM_TEXT;
+
+public class EditItemsActivity extends AppCompatActivity {
+
+    // track edit text
+    EditText etItemText;
+    //position of edited item in list
+    int position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_items);
+        // resolve edit text rom layout
+        etItemText = (EditText) findViewById(R.id.etItemText);
+        // set edit text value from intent extra
+        etItemText.setText(getIntent().getStringExtra(ITEM_TEXT));
+        // update position from intent extra
+        position = getIntent().getIntExtra(ITEM_POSITION, 0);
+        // update the title bar of the activity
+        getSupportActionBar().setTitle("Edit Item");
+
+    }
+
+    // handler for save button
+    public void onSaveItem (View v){
+        // prepare new intend for result
+        Intent i = new Intent();
+        // pass updated item text as extra
+        i.putExtra(ITEM_TEXT, etItemText.getText().toString());
+        // pass original position as extra
+        i.putExtra(ITEM_POSITION, position);
+        // set the intent as the result of the activity
+        setResult(RESULT_OK, i);
+        //close the activity and redirect to main
+        finish();
+
+    }
+}
